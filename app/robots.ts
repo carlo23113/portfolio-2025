e68988aab5696ec687aa5df81a04dac3252cbb13
carlo23113 <jcespiritu94@gmail.com>
2025-10-29 03:46:03 +0800

    fixed seo

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://jancarloespiritu.com";
+  const baseUrl = "https://www.jancarloespiritu.com";
 
   return {
     rules: [
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://jancarloespiritu.com";
+  const baseUrl = "https://www.jancarloespiritu.com";
 
   return [
     {
